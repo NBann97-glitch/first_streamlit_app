7441ed8e184bdbfc00f697ba598873dd6521171b
NBann97-glitch <105641907+NBann97-glitch@users.noreply.github.com>
2022-05-17 11:31:38 +0100

    Update streamlit_app.py
    Added new header with emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avacado Toast')
+
+
+streamlit.header('🍌🍏Build your own fruit smoothie🍉🥭')
